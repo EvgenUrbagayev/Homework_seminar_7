50df7b45bcd1e284dd6f5405b46fcca693ec63e4
Evgeniy <eurbagayev@mail.com>
2022-10-19 22:04:15 +0300

    Двумерный массив размером m*n

---
+++ Task_47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+
+using static System.Console;
+Clear();
+
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+
+
+
+
+double[,] matrix = new double[3, 4];
+
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
